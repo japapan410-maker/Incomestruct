98f1bcc3c3eb69e8cca34dfc22e4e0df28a8e25a
japapan410-maker <japapan410@gmail.com>
2026-09-11 21:40:18 +0700

    Update MainActivity.java

--- app/src/main/java/com/helmi/incomestruct/MainActivity.java
+++ app/src/main/java/com/helmi/incomestruct/MainActivity.java
@@ -3,24 +3,23 @@ package com.helmi.incomestruct;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         FrameLayout container = new FrameLayout(this);
-        container.setId(android.R.id.content);
+        container.setId(android.view.View.generateViewId());
 
         setContentView(container);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(android.R.id.content, new BerandaFragment())
+                    .replace(container.getId(), new BerandaFragment())
                     .commit();
         }
     }
